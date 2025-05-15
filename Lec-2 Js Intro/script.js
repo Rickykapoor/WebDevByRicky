234d78045dc7920650a28f4b63dcec77bfd07f13
Rickykapoor <77842677+Rickykapoor@users.noreply.github.com>
2025-05-16 01:10:29 +0530

    Code folder html & CSS again studied

--- Lec-2 Js Intro/script.js
+++ Lec-2 Js Intro/script.js
@@ -1,7 +1,33 @@
-const a = 22
-const b = 30
-const c = a + b
-console.log("The sum of a and b is: " + c)
+let random;
+random = 1227
+
+const random1 = 3441;
+
+let accountEmail = "hitesh8@google.com"
+
+// random = 3
+// console.log(random)
+
+// let ki value change ho sakti hai
+// random1 = 8
+// console.log(random1)
+
+// const a = 22
+// const b = 30
+// const c = a + b
+// console.log("The sum of a and b is: " + c)
+
+// const a = "22"
+// const b = 30
+// const c = a + b
+// console.log("The sum of a and b is: " + c)
+
+// const a = 22
+// const b = 30
+// const c = a + b
+// console.log("The sum of a and b is: " , c)
+
+
 
 /*
 This is a multi-line comment
@@ -11,48 +37,55 @@ This is a multi-line comment
 */
 
 // This is a single line comment
-// Variable Declaration
-const name = "John Doe"
-const age = 30
+
+
+
+
+// // Variable Declaration
+// const name = "John Doe"
+// const age = 30
+
+// boolean
 const isStudent = true
-const hobbies = ["reading", "sports", "music"]
-const address = {
-  street: "123 Main St",
-  city: "New York",
-  state: "NY"
-}
 
-//substraction
-const d = 50
-const e = 20
-const f = d - e
-console.log("The difference of d and e is: " + f)
-// multiplication
-const g = 5
-const h = 10
-const i = g * h
-console.log("The product of g and h is: " + i)
-// division
-const j = 100
-const k = 25
-const l = j / k
-console.log("The quotient of j and k is: " + l)
-// modulus
-const m = 10
-const n = 3
-const o = m % n
-console.log("The remainder of m divided by n is: " + o)
-// increment
-let p = 5
-p++
-console.log("The value of p after incrementing is: " + p)
-// decrement
-let q = 10
-q--
-console.log("The value of q after decrementing is: " + q)
+// const hobbies = ["reading", "sports", "music"]
+// const address = {
+//   street: "123 Main St",
+//   city: "New York",
+//   state: "NY"
+// }
 
-// operators
-const x = 5
-const y = 10
-const z = x + y
-console.log("The sum of x and y is: " + z)
+// //substraction
+// const d = 50
+// const e = 20
+// const f = d - e
+// console.log("The difference of d and e is: " + f)
+// // multiplication
+// const g = 5
+// const h = 10
+// const i = g * h
+// console.log("The product of g and h is: " + i)
+// // division
+// const j = 100
+// const k = 25
+// const l = j / k
+// console.log("The quotient of j and k is: " + l)
+// // modulus
+// const m = 10
+// const n = 3
+// const o = m % n
+// console.log("The remainder of m divided by n is: " + o)
+// // increment
+// let p = 5
+// p++
+// console.log("The value of p after incrementing is: " + p)
+// // decrement
+// let q = 10
+// q--
+// // console.log("The value of q after decrementing is: " + q)
+
+// // operators
+// const x = 5
+// const y = 10
+// const z = x + y
+// // console.log("The sum of x and y is: " + z)
